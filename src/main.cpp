aa7ee9b3eef574250a1da593424833769309af0f
Zachary Stevens <zed2808@gmail.com>
2014-08-02 13:37:15 -0400

    Added character name input window.
    Also added .gitignore file to ignore executable.

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,34 @@
 #include "player.h"
 
 int main() {
+	char name[34];
+
 	WINDOW *win_stats;
 	int win_stats_height = 5;
 	int win_stats_width = 36;
 
 	Player player;
-	//player.set_name("Default Player");
 	player.set_health(87);
 	player.set_mana(9);
 	player.set_stamina(59);
 
 	initscr();
 	start_color();
-	curs_set(0);
 
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	init_pair(2, COLOR_BLUE, COLOR_BLACK);
 	init_pair(3, COLOR_GREEN, COLOR_BLACK);
 
 	refresh();
+
+	WINDOW *win_name = create_newwin(4, 36, 0, 10);
+	wattron(win_name, A_BOLD);
+	mvwprintw(win_name, 1, 1, "Enter your name (up to 34 chars).");
+	wattroff(win_name, A_BOLD);
+	curs_set(1);
+	mvwgetstr(win_name, 2, 1, name);
+	player.set_name(name);
+	curs_set(0);
 
 	win_stats = create_newwin(win_stats_height, win_stats_width, 5, 10);
 	draw_stats(win_stats, player.get_name(), player.get_health(), player.get_mana(), player.get_stamina());
