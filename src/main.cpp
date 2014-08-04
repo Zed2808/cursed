df3e3463f88d284c88a37cc2b9bd8a8f62d43f04
Zachary Stevens <zed2808@gmail.com>
2014-08-03 20:13:40 -0400

    Chnaged layout of example screens.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "wolf.h"
 
 int main() {
-	char name[34];
+	char name[24];
 
 	int win_stats_height = 5;
 	int win_stats_width = 39;
@@ -19,9 +19,9 @@ int main() {
 	refresh();
 
 	/* Get player's name */
-	WINDOW *win_name = create_newwin(4, 36, 5, 10);
+	WINDOW *win_name = create_newwin(4, 36, (LINES-4)/2, (COLS-36)/2);
 	wattron(win_name, A_BOLD);
-	mvwprintw(win_name, 1, 1, "Enter your name (up to 34 chars).");
+	mvwprintw(win_name, 1, 1, "Enter your name (up to 24 chars).");
 	wattroff(win_name, A_BOLD);
 	curs_set(1);
 	mvwgetstr(win_name, 2, 1, name);
@@ -37,14 +37,14 @@ int main() {
 	refresh();
 
 	/* Display player stats window */
-	WINDOW *win_player = create_newwin(win_stats_height, win_stats_width, 5, 10);
+	WINDOW *win_player = create_newwin(win_stats_height, win_stats_width, LINES-win_stats_height, 0);
 	draw_player_stats(win_player, player);
 
 	/* Create wolf object that's level 4 */
 	Wolf wolf1(4);
 
 	/* Display wolf stats window */
-	WINDOW *win_wolf1 = create_newwin(win_stats_height, win_stats_width, 11, 10);
+	WINDOW *win_wolf1 = create_newwin(win_stats_height, win_stats_width, LINES-win_stats_height, COLS-win_stats_width);
 	draw_monster_stats(win_wolf1, wolf1);
 
 	getch();
