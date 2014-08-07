57c8ff8ca3a5a51387c94be518c7a33dd92fb9a8
Zachary Stevens <zed2808@gmail.com>
2014-08-07 17:46:30 -0400

    Added attributes to Character class.

--- src/character.cpp
+++ src/character.cpp
@@ -1,66 +1,39 @@
 #include <string>
 #include "character.h"
 
-void Character::set_name(std::string n) {
-	name = n;
-}
+/* Sets & Gets for current stats */
+void Character::set_name(std::string n) { name = n; }
+std::string Character::get_name() {	return name; }
+void Character::set_level(int l) { level = l; }
+int Character::get_level() { return level; }
 
-void Character::set_level(int l) {
-	level = l;
-}
+void Character::set_health(int h) { health = h; }
+int Character::get_health() { return health; }
+void Character::set_mana(int m) { mana = m; }
+int Character::get_mana() { return mana; }
+void Character::set_stamina(int s) { stamina = s; }
+int Character::get_stamina() { return stamina; }
 
-void Character::set_maxhealth(int h) {
-	maxhealth = h;
-}
+/* Sets & Gets for main attributes */
+void Character::set_endurance(int e) { endurance = e; }
+int Character::get_endurance() { return endurance; }
+void Character::set_intelligence(int i) { intelligence = i; }
+int Character::get_intelligence() { return intelligence; }
+void Character::set_agility(int a) { agility = a; }
+int Character::get_agility() { return agility; }
+void Character::set_personality(int p) { personality = p; }
+int Character::get_personality() { return personality; }
+void Character::set_perception(int p) { perception = p; }
+int Character::get_perception() { return perception; }
+void Character::set_luck(int l) { luck = l; }
+int Character::get_luck() { return luck; }
 
-void Character::set_health(int h) {
-	health = h;
-}
-
-void Character::set_maxmana(int m) {
-	maxmana = m;
-}
-
-void Character::set_mana(int m) {
-	mana = m;
-}
-
-void Character::set_maxstamina(int s) {
-	maxstamina = s;
-}
-
-void Character::set_stamina(int s) {
-	stamina = s;
-}
-
-std::string Character::get_name() {
-	return name;
-}
-
-int Character::get_level() {
-	return level;
-}
-
-int Character::get_maxhealth() {
-	return maxhealth;
-}
-
-int Character::get_health() {
-	return health;
-}
-
-int Character::get_maxmana() {
-	return maxmana;
-}
-
-int Character::get_mana() {
-	return mana;
-}
-
-int Character::get_maxstamina() {
-	return maxstamina;
-}
-
-int Character::get_stamina() {
-	return stamina;
-}
+/* Sets & Gets for derived attributes */
+void Character::set_maxhealth(int h) { maxhealth = h; }
+int Character::get_maxhealth() { return maxhealth; }
+void Character::set_maxmana(int m) { maxmana = m; }
+int Character::get_maxmana() { return maxmana; }
+void Character::set_maxstamina(int s) { maxstamina = s; }
+int Character::get_maxstamina() { return maxstamina; }
+void Character::set_carryweight(int c) { carryweight = c; }
+int Character::get_carryweight() { return carryweight; }
--- src/character.h
+++ src/character.h
@@ -5,31 +5,65 @@
 
 class Character {
 	protected:
+		/* Current Stats */
 		std::string name;
 		int level;
-		int maxhealth;
+
 		int health;
-		int maxmana;
 		int mana;
-		int maxstamina;
 		int stamina;
+
+		/* Main Attributes */
+		int endurance;		/* Affects health */
+		int intelligence;	/* Affects mana */
+		int agility;		/* Affects stamina */
+		int strength;		/* Affects carry weight */
+		int personality;
+		int perception;
+		int luck;
+
+		/* Derived Attributes */
+		int maxhealth;
+		int maxmana;
+		int maxstamina;
+		int carryweight;
 	public:
+		/* Sets & Gets for current stats */
 		void set_name(std::string n);
-		void set_level(int l);
-		void set_maxhealth(int h);
-		void set_health(int h);
-		void set_maxmana(int m);
-		void set_mana(int m);
-		void set_maxstamina(int s);
-		void set_stamina(int s);
 		std::string get_name();
+		void set_level(int l);
 		int get_level();
-		int get_maxhealth();
+
+		void set_health(int h);
 		int get_health();
-		int get_maxmana();
+		void set_mana(int m);
 		int get_mana();
-		int get_maxstamina();
+		void set_stamina(int s);
 		int get_stamina();
+
+		/* Sets & Gets for main attributes */
+		void set_endurance(int e);
+		int get_endurance();
+		void set_intelligence(int i);
+		int get_intelligence();
+		void set_agility(int a);
+		int get_agility();
+		void set_personality(int p);
+		int get_personality();
+		void set_perception(int p);
+		int get_perception();
+		void set_luck(int l);
+		int get_luck();
+
+		/* Sets & Gets for derived attributes */
+		void set_maxhealth(int h);
+		int get_maxhealth();
+		void set_maxmana(int m);
+		int get_maxmana();
+		void set_maxstamina(int s);
+		int get_maxstamina();
+		void set_carryweight(int c);
+		int get_carryweight();
 };
 
 #endif
