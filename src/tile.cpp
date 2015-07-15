38080e9bb104e93872ae92489b24711f27044d9c
Zachary Stevens <zed2808@gmail.com>
2015-07-14 21:34:36 -0400

    Added tile attributes and a couple starter tiles

--- src/tile.cpp
+++ src/tile.cpp
@@ -1,5 +1,32 @@
+#ifdef __MINGW32__
+#include <curses.h>
+#endif
+
+#ifdef __linux__
+#include <ncurses.h>
+#endif
+
 #include "tile.hpp"
 
+#include <string>
+
 Tile::Tile() {
-    
+    name = "empty";
+    symbol = '.';
+    fgcolor = COLOR_WHITE;
+    bgcolor = COLOR_BLACK;
+}
+
+Grass::Grass() {
+    name = "grass";
+    symbol = '.';
+    fgcolor = COLOR_GREEN;
+    bgcolor = COLOR_BLACK;
+}
+
+Stone::Stone() {
+    name = "stone";
+    symbol = '.';
+    fgcolor = COLOR_WHITE;
+    bgcolor = COLOR_BLACK;
 }
--- src/tile.hpp
+++ src/tile.hpp
@@ -1,9 +1,25 @@
 #ifndef TILE_H
 #define TILE_H
 
+#include <string>
+
 class Tile {
     public:
         Tile();
+        std::string name;
+        char symbol;
+        int fgcolor;
+        int bgcolor;
+};
+
+class Grass : public Tile {
+    public:
+        Grass();
+};
+
+class Stone : public Tile {
+    public:
+        Stone();
 };
 
 #endif
