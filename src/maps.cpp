a00b49de9745ff545808dd640716029540718213
Zachary Stevens <zed2808@gmail.com>
2015-08-14 01:42:25 -0400

    Converted testmap to new format, began map reading rewrite

--- src/maps.cpp
+++ src/maps.cpp
@@ -11,20 +11,20 @@
 #include "maps.hpp"
 #include "tile.hpp"
 
-#define MAPHEIGHT 16
-#define MAPWIDTH 16
+#define TEMPMAPHEIGHT 16
+#define TEMPMAPWIDTH 16
 
 // TODO: Variable sized maps
 
 Map::Map() {
-    for(int row = 0; row < MAPHEIGHT; row++) {
-        for(int col = 0; col < MAPWIDTH; col++) {
+    for(int row = 0; row < TEMPMAPHEIGHT; row++) {
+        for(int col = 0; col < TEMPMAPWIDTH; col++) {
             tiles[row][col] = Tile();
         }
     }
 
-    for(int row = 0; row < MAPHEIGHT; row++) {
-        for(int col = 0; col < MAPWIDTH; col++) {
+    for(int row = 0; row < TEMPMAPHEIGHT; row++) {
+        for(int col = 0; col < TEMPMAPWIDTH; col++) {
             characters[row][col] = Character();
         }
     }
@@ -49,7 +49,7 @@ void Map::move_character(Character &character, int key) {
         case KEY_DOWN:
             if(characters[character.row + 1][character.col].name != "") {
                 // Interact
-            } else if(character.row < MAPHEIGHT - 1 && tiles[character.row + 1][character.col].walkable) {
+            } else if(character.row < TEMPMAPHEIGHT - 1 && tiles[character.row + 1][character.col].walkable) {
                 characters[character.row][character.col] = Character();
                 place_character(character.row + 1, character.col, character);
             }
@@ -65,7 +65,7 @@ void Map::move_character(Character &character, int key) {
         case KEY_RIGHT:
             if(characters[character.row][character.col + 1].name != "") {
                 // Interact
-            } else if(character.col < MAPWIDTH - 1 && tiles[character.row][character.col + 1].walkable) {
+            } else if(character.col < TEMPMAPWIDTH - 1 && tiles[character.row][character.col + 1].walkable) {
                 characters[character.row][character.col] = Character();
                 place_character(character.row, character.col + 1, character);
             }
@@ -77,19 +77,23 @@ void load_map(Map &map, const char *name) {
     unsigned char *buffer; // Pointer to buffered data
     FILE *file = NULL;      // File pointer
 
+    char mapname[32];
+    char mapheight[1];
+    char mapwidth[1];
+
     char dir[32];
     strcpy(dir, "data/maps/");
     strcat(dir, name);
 
     file = fopen(dir, "rb");
 
-    buffer = new unsigned char[MAPHEIGHT * MAPWIDTH];
+    buffer = new unsigned char[1024];
 
-    fread(buffer, MAPHEIGHT * MAPWIDTH, 1, file);
+    fread(buffer, TEMPMAPHEIGHT * TEMPMAPWIDTH, 1, file);
 
     int index = 0;
-    for(int row = 0; row < MAPHEIGHT; row++) {
-        for(int col = 0; col < MAPWIDTH; col++) {
+    for(int row = 0; row < TEMPMAPHEIGHT; row++) {
+        for(int col = 0; col < TEMPMAPWIDTH; col++) {
             map.tiles[row][col] = get_tile_from_id(buffer[index]);
             index++;
         }
