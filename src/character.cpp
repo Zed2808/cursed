59c6e77e57ea25a9a91ee284136fe501386deb55
Zachary Stevens <zed2808@gmail.com>
2015-08-29 00:29:34 -0400

    Tidied up indentation in character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -19,23 +19,82 @@ Character::Character() {
 }
 
 /* Sets & Gets for main attributes */
-void Character::set_endurance(int e) { endurance = e; maxhealth = endurance * 20; health = maxhealth; }
-int Character::get_endurance() { return endurance; }
-void Character::set_intelligence(int i) { intelligence = i; maxmana = intelligence * 20; mana = maxmana; }
-int Character::get_intelligence() { return intelligence; }
-void Character::set_agility(int a) { agility = a; maxstamina = agility * 10; stamina = maxstamina; }
-int Character::get_agility() { return agility; }
-void Character::set_strength(int s) { strength = s; carryweight = strength * 20; }
-int Character::get_strength() { return strength; }
-void Character::set_personality(int p) { personality = p; }
-int Character::get_personality() { return personality; }
-void Character::set_perception(int p) { perception = p; }
-int Character::get_perception() { return perception; }
-void Character::set_luck(int l) { luck = l; }
-int Character::get_luck() { return luck; }
+void Character::set_endurance(int e) {
+    endurance = e;
+    maxhealth = endurance * 20;
+    health = maxhealth;
+}
+
+int Character::get_endurance() {
+    return endurance;
+}
+
+void Character::set_intelligence(int i) {
+    intelligence = i;
+    maxmana = intelligence * 20;
+    mana = maxmana;
+}
+
+int Character::get_intelligence() {
+    return intelligence;
+}
+
+void Character::set_agility(int a) {
+    agility = a;
+    maxstamina = agility * 10;
+    stamina = maxstamina;
+}
+
+int Character::get_agility() {
+    return agility;
+}
+
+void Character::set_strength(int s) {
+    strength = s;
+    carryweight = strength * 20;
+}
+
+int Character::get_strength() {
+    return strength;
+}
+
+void Character::set_personality(int p) {
+    personality = p;
+}
+
+int Character::get_personality() {
+    return personality;
+}
+
+void Character::set_perception(int p) {
+    perception = p;
+}
+
+int Character::get_perception() {
+    return perception;
+}
+
+void Character::set_luck(int l) {
+    luck = l;
+}
+
+int Character::get_luck() {
+    return luck;
+}
 
 /* Gets for derived attributes */
-int Character::get_maxhealth() { return maxhealth; }
-int Character::get_maxmana() { return maxmana; }
-int Character::get_maxstamina() { return maxstamina; }
-int Character::get_carryweight() { return carryweight; }
+int Character::get_maxhealth() {
+    return maxhealth;
+}
+
+int Character::get_maxmana() {
+    return maxmana;
+}
+
+int Character::get_maxstamina() {
+    return maxstamina;
+}
+
+int Character::get_carryweight() {
+    return carryweight;
+}
